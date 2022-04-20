05ac4eb10429f55894befd1a13b833661e34ac83
alepbloyd <alexanderleeboyd@gmail.com>
2022-04-20 19:27:00 -0400

    Add a good friend

--- friends.rb
+++ friends.rb
@@ -1,7 +1,13 @@
 best_friend = "Jamie"
 
+good_friend = "Bob"
+
 if best_friend == "Jamie"
   puts "You are cool"
 else
   puts "You are okay"
+end
+
+if good_friend == "Bob"
+  puts "You are cool too"
 end
